c6fee8ce532e4ab0a35008d3359b559d2b2d75c0
atk-sz <65167734+atk-sz@users.noreply.github.com>
2021-03-04 16:19:46 +0530

    done with that fill in the field pop-error for google sign in

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,7 +4,7 @@ import './custom-button.style.scss';
 const CustomButtom = ({children , isGoogleSignIn, ...otherProps})=>{
     // console.log({...otherProps})
     return (
-    <button className={`${isGoogleSignIn?'google-sign-in':''} custom-button`}>
+    <button className={`${isGoogleSignIn?'google-sign-in':''} custom-button`} type={`${isGoogleSignIn?'button':'submit'}`}>
         {children}
     </button>
 )}
